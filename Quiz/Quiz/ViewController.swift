098bf08cbbbd3c812f62c9e716f3ceb9f325dd4a
akirkman10 <akirkman@highpoint.edu>
2017-01-18 13:22:48 -0500

    Add files via upload

---
+++ Quiz/Quiz/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  Quiz
+//
+//  Created by Alexis Kirkman on 1/11/17.
+//  Copyright © 2017 Alexis Kirkman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+   @IBOutlet var questionLabel: UILabel!
+   @IBOutlet var answerLabel: UILabel!
+   
+   let questions: [String] = [
+      "What is 7+7?",
+      "What is the capital of Vermont?",
+      "What is cognac made from?"
+   ]
+   let answers: [String] = [
+      "14",
+      "Montpelier",
+      "Grapes"
+   ]
+   var currentQuestionIndex: Int=0
+   @IBAction func showNextQuestion(_ sender: UIButton){
+      currentQuestionIndex += 1
+      if currentQuestionIndex == questions.count{
+         currentQuestionIndex = 0
+      }
+      let question: String = questions[currentQuestionIndex]
+      questionLabel.text = question
+      answerLabel.text = "???"
+   }
+   
+   @IBAction func showAnswer(_ sender: UIButton){
+      let answer: String = answers[currentQuestionIndex]
+      answerLabel.text = answer
+   }
+
+
+}
+
